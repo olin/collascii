8194292e2ed271fd96e93f2bb42fe839e1e22000
Adam Novotny <adam.novotny55@gmail.com>
2019-05-04 22:41:08 -0400

    formatting fix

--- src/server.c
+++ src/server.c
@@ -2,27 +2,27 @@
 
     Server for CollASCII
 
-    mostly copied from: https://github.com/yorickdewid/Chat-Server/blob/master/chat_server.c
+    mostly copied from:
+   https://github.com/yorickdewid/Chat-Server/blob/master/chat_server.c
 */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-#include <sys/socket.h>
 #include <netdb.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <netinet/in.h>
 #include <pthread.h>
-#include <sys/types.h>
 #include <signal.h>
+#include <sys/types.h>
 
 // #include "network.h"
 #include "canvas.h"
-
 
 static _Atomic unsigned int cli_count = 0;
 static int uid = 10;
@@ -32,10 +32,10 @@ static int uid = 10;
 
 /* Client structure */
 typedef struct {
-    struct sockaddr_in addr; /* Client remote address */
-    int connfd;              /* Connection file descriptor */
-    int uid;                 /* Client unique identifier */
-    char name[32];           /* Client name */
+  struct sockaddr_in addr; /* Client remote address */
+  int connfd;              /* Connection file descriptor */
+  int uid;                 /* Client unique identifier */
+  char name[32];           /* Client name */
 } client_t;
 
 client_t *clients[MAX_CLIENTS];
@@ -45,148 +45,144 @@ pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 Canvas *canvas;
 
 /* Add client to queue */
-void queue_add(client_t *cl){
-    pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < MAX_CLIENTS; ++i) {
-        if (!clients[i]) {
-            clients[i] = cl;
-            break;
-        }
+void queue_add(client_t *cl) {
+  pthread_mutex_lock(&clients_mutex);
+  for (int i = 0; i < MAX_CLIENTS; ++i) {
+    if (!clients[i]) {
+      clients[i] = cl;
+      break;
     }
-    pthread_mutex_unlock(&clients_mutex);
+  }
+  pthread_mutex_unlock(&clients_mutex);
 }
 
 /* Delete client from queue */
-void queue_delete(int uid){
-    pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < MAX_CLIENTS; ++i) {
-        if (clients[i]) {
-            if (clients[i]->uid == uid) {
-                clients[i] = NULL;
-                break;
-            }
-        }
+void queue_delete(int uid) {
+  pthread_mutex_lock(&clients_mutex);
+  for (int i = 0; i < MAX_CLIENTS; ++i) {
+    if (clients[i]) {
+      if (clients[i]->uid == uid) {
+        clients[i] = NULL;
+        break;
+      }
     }
-    pthread_mutex_unlock(&clients_mutex);
+  }
+  pthread_mutex_unlock(&clients_mutex);
 }
 
 /* Send message to all clients but the sender */
-void send_message(char *s, int uid){
-    pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < MAX_CLIENTS; ++i) {
-        if (clients[i]) {
-            if (clients[i]->uid != uid) {
-                if (write(clients[i]->connfd, s, strlen(s)) < 0) {
-                    perror("Write to descriptor failed");
-                    break;
-                }
-            }
+void send_message(char *s, int uid) {
+  pthread_mutex_lock(&clients_mutex);
+  for (int i = 0; i < MAX_CLIENTS; ++i) {
+    if (clients[i]) {
+      if (clients[i]->uid != uid) {
+        if (write(clients[i]->connfd, s, strlen(s)) < 0) {
+          perror("Write to descriptor failed");
+          break;
         }
+      }
     }
-    pthread_mutex_unlock(&clients_mutex);
+  }
+  pthread_mutex_unlock(&clients_mutex);
 }
 
 /* Send message to sender */
-void send_message_self(const char *s, int connfd){
-    if (write(connfd, s, strlen(s)) < 0) {
-        perror("Write to descriptor failed");
-        exit(-1);
-    }
+void send_message_self(const char *s, int connfd) {
+  if (write(connfd, s, strlen(s)) < 0) {
+    perror("Write to descriptor failed");
+    exit(-1);
+  }
 }
 
 /* Send message to client */
-void send_message_client(char *s, int uid){
-    pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < MAX_CLIENTS; ++i){
-        if (clients[i]) { 
-            if (clients[i]->uid == uid) {
-                if (write(clients[i]->connfd, s, strlen(s))<0) {
-                    perror("Write to descriptor failed");
-                    break;
-                }
-            }
+void send_message_client(char *s, int uid) {
+  pthread_mutex_lock(&clients_mutex);
+  for (int i = 0; i < MAX_CLIENTS; ++i) {
+    if (clients[i]) {
+      if (clients[i]->uid == uid) {
+        if (write(clients[i]->connfd, s, strlen(s)) < 0) {
+          perror("Write to descriptor failed");
+          break;
         }
+      }
     }
-    pthread_mutex_unlock(&clients_mutex);
+  }
+  pthread_mutex_unlock(&clients_mutex);
 }
 
 /* Strip CRLF */
-void strip_newline(char *s){
-    while (*s != '\0') {
-        if (*s == '\r' || *s == '\n') {
-            *s = '\0';
-        }
-        s++;
+void strip_newline(char *s) {
+  while (*s != '\0') {
+    if (*s == '\r' || *s == '\n') {
+      *s = '\0';
     }
+    s++;
+  }
 }
 
 /* Print ip address */
-void print_client_addr(struct sockaddr_in addr){
-    printf("%d.%d.%d.%d",
-        addr.sin_addr.s_addr & 0xff,
-        (addr.sin_addr.s_addr & 0xff00) >> 8,
-        (addr.sin_addr.s_addr & 0xff0000) >> 16,
-        (addr.sin_addr.s_addr & 0xff000000) >> 24);
+void print_client_addr(struct sockaddr_in addr) {
+  printf("%d.%d.%d.%d", addr.sin_addr.s_addr & 0xff,
+         (addr.sin_addr.s_addr & 0xff00) >> 8,
+         (addr.sin_addr.s_addr & 0xff0000) >> 16,
+         (addr.sin_addr.s_addr & 0xff000000) >> 24);
 }
-
 
 /* Handle all communication with the client */
-void *handle_client(void *arg){
-    char buff_out[BUFFER_SZ];
-    char buff_in[BUFFER_SZ / 2];
-    int rlen;
+void *handle_client(void *arg) {
+  char buff_out[BUFFER_SZ];
+  char buff_in[BUFFER_SZ / 2];
+  int rlen;
 
-    cli_count++;
-    client_t *cli = (client_t *)arg;
+  cli_count++;
+  client_t *cli = (client_t *)arg;
 
-    printf("<< accept ");
-    print_client_addr(cli->addr);
-    printf(" referenced by %d\n", cli->uid);
+  printf("<< accept ");
+  print_client_addr(cli->addr);
+  printf(" referenced by %d\n", cli->uid);
 
-    
-    send_message_self("1000 1000\n", cli->connfd);
+  send_message_self("1000 1000\n", cli->connfd);
 
-    /* Receive input from client */
-    while ((rlen = read(cli->connfd, buff_in, sizeof(buff_in) - 1)) > 0) {
-        buff_in[rlen] = '\0';
-        buff_out[0] = '\0';
-        strip_newline(buff_in);
+  /* Receive input from client */
+  while ((rlen = read(cli->connfd, buff_in, sizeof(buff_in) - 1)) > 0) {
+    buff_in[rlen] = '\0';
+    buff_out[0] = '\0';
+    strip_newline(buff_in);
 
-        /* Ignore empty buffer */
-        if (!strlen(buff_in)) {
-            continue;
-        }
-
-        /* Special options */
-        if (buff_in[0] == '/') {
-            char *command;
-            command = strtok(buff_in," ");
-            if (!strcmp(command, "/quit")) {
-                break;
-            }
-        } else {
-            /* Send message */
-            snprintf(buff_out, sizeof(buff_out), "[%s] %s\r\n", cli->name, buff_in);
-            send_message(buff_out, cli->uid);
-            printf("%s\n", buff_out);
-        }
+    /* Ignore empty buffer */
+    if (!strlen(buff_in)) {
+      continue;
     }
 
-    /* Close connection */
-    close(cli->connfd);
+    /* Special options */
+    if (buff_in[0] == '/') {
+      char *command;
+      command = strtok(buff_in, " ");
+      if (!strcmp(command, "/quit")) {
+        break;
+      }
+    } else {
+      /* Send message */
+      snprintf(buff_out, sizeof(buff_out), "[%s] %s\r\n", cli->name, buff_in);
+      send_message(buff_out, cli->uid);
+      printf("%s\n", buff_out);
+    }
+  }
 
-    /* Delete client from queue and yield thread */
-    queue_delete(cli->uid);
-    printf("<< quit ");
-    print_client_addr(cli->addr);
-    printf(" referenced by %d\n", cli->uid);
-    free(cli);
-    cli_count--;
-    pthread_detach(pthread_self());
+  /* Close connection */
+  close(cli->connfd);
 
-    return NULL;
+  /* Delete client from queue and yield thread */
+  queue_delete(cli->uid);
+  printf("<< quit ");
+  print_client_addr(cli->addr);
+  printf(" referenced by %d\n", cli->uid);
+  free(cli);
+  cli_count--;
+  pthread_detach(pthread_self());
+
+  return NULL;
 }
-
 
 /* listenandloop
  *
@@ -270,8 +266,8 @@ int listenandloop(char *port)
         socklen_t addr_size;
         int incoming_fd;
         addr_size = sizeof(their_addr);
-        incoming_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
-        if (incoming_fd == -1)
+        incoming_fd = accept(sockfd, (struct sockaddr *)&their_addr,
+&addr_size); if (incoming_fd == -1)
         {
             perror("accept");
             exit(1);
@@ -291,67 +287,66 @@ int listenandloop(char *port)
 }
 */
 
-int main()
-{
-    canvas = canvas_new_blank(1000, 1000);
-    
-    int listenfd = 0, connfd = 0;
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in cli_addr;
-    pthread_t tid;
+int main() {
+  canvas = canvas_new_blank(1000, 1000);
 
-    /* Socket settings */
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(5000);
+  int listenfd = 0, connfd = 0;
+  struct sockaddr_in serv_addr;
+  struct sockaddr_in cli_addr;
+  pthread_t tid;
 
-    /* Ignore pipe signals */
-    signal(SIGPIPE, SIG_IGN);
+  /* Socket settings */
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_addr.sin_port = htons(5000);
 
-    /* Bind */
-    if (bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Socket binding failed");
-        return EXIT_FAILURE;
+  /* Ignore pipe signals */
+  signal(SIGPIPE, SIG_IGN);
+
+  /* Bind */
+  if (bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    perror("Socket binding failed");
+    return EXIT_FAILURE;
+  }
+
+  /* Listen */
+  if (listen(listenfd, 10) < 0) {
+    perror("Socket listening failed");
+    return EXIT_FAILURE;
+  }
+
+  printf("<[ SERVER STARTED ]>\n");
+
+  /* Accept clients */
+  while (1) {
+    socklen_t clilen = sizeof(cli_addr);
+    connfd = accept(listenfd, (struct sockaddr *)&cli_addr, &clilen);
+
+    /* Check if max clients is reached */
+    if ((cli_count + 1) == MAX_CLIENTS) {
+      printf("<< max clients reached\n");
+      printf("<< reject ");
+      print_client_addr(cli_addr);
+      printf("\n");
+      close(connfd);
+      continue;
     }
 
-    /* Listen */
-    if (listen(listenfd, 10) < 0) {
-        perror("Socket listening failed");
-        return EXIT_FAILURE;
-    }
+    /* Client settings */
+    client_t *cli = (client_t *)malloc(sizeof(client_t));
+    cli->addr = cli_addr;
+    cli->connfd = connfd;
+    cli->uid = uid++;
+    sprintf(cli->name, "%d", cli->uid);
 
-    printf("<[ SERVER STARTED ]>\n");
+    /* Add client to the queue and fork thread */
+    queue_add(cli);
+    pthread_create(&tid, NULL, &handle_client, (void *)cli);
 
-    /* Accept clients */
-    while (1) {
-        socklen_t clilen = sizeof(cli_addr);
-        connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
+    /* Reduce CPU usage */
+    sleep(1);
+  }
 
-        /* Check if max clients is reached */
-        if ((cli_count + 1) == MAX_CLIENTS) {
-            printf("<< max clients reached\n");
-            printf("<< reject ");
-            print_client_addr(cli_addr);
-            printf("\n");
-            close(connfd);
-            continue;
-        }
-
-        /* Client settings */
-        client_t *cli = (client_t *)malloc(sizeof(client_t));
-        cli->addr = cli_addr;
-        cli->connfd = connfd;
-        cli->uid = uid++;
-        sprintf(cli->name, "%d", cli->uid);
-
-        /* Add client to the queue and fork thread */
-        queue_add(cli);
-        pthread_create(&tid, NULL, &handle_client, (void*)cli);
-
-        /* Reduce CPU usage */
-        sleep(1);
-    }
-
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
