26af8f2fe7055c10fa82227c04b94acd2fd4ed7c
Evan New-Schmidt <evan@new-schmidt.com>
2019-05-06 14:07:56 -0400

    Reorganize canvas.h

--- src/canvas.h
+++ src/canvas.h
@@ -18,23 +18,28 @@ int canvas_isin_y(Canvas *canvas, int y);
 int canvas_isin_x(Canvas *canvas, int x);
 int canvas_isin_yx(Canvas *canvas, int y, int x);
 int canvas_isin_i(Canvas *canvas, int i);
+int canvas_eq(Canvas *a, Canvas *b);
 
-int canvas_ldcanvasyx(Canvas *dest, Canvas *source, int y, int x);
-int canvas_ldcanvasyxc(Canvas *dest, Canvas *source, int y, int x,
-                       char transparent);
-
-int canvas_resize(Canvas **orig, int newrows, int newcols);
 void canvas_scharyx(Canvas *canvas, int y, int x, char c);
 void canvas_schari(Canvas *canvas, int i, char c);
 char canvas_gcharyx(Canvas *canvas, int y, int x);
 char canvas_gchari(Canvas *canvas, int i);
+
+int canvas_ldcanvasyx(Canvas *dest, Canvas *source, int y, int x);
+int canvas_ldcanvasyxc(Canvas *dest, Canvas *source, int y, int x,
+                       char transparent);
+int canvas_resize(Canvas **orig, int newrows, int newcols);
+
+int canvas_ldstryx(Canvas *canvas, char *str, int y, int x);
+int canvas_ldstr(Canvas *canvas, char *str);
 int canvas_load_str(Canvas *canvas, char *str);
+
+Canvas *canvas_readf(FILE *f);
 int canvas_fprint(FILE *stream, Canvas *canvas);
 int canvas_fprint_trim(FILE *stream, Canvas *canvas);
 int canvas_print(Canvas *canvas);
+
 int canvas_serialize(Canvas *canvas, char *buf);
 void canvas_deserialize(char *bytes, Canvas *canvas);
-int canvas_eq(Canvas *a, Canvas *b);
-Canvas *canvas_readf(FILE *f);
 
 #endif
