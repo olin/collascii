f0f5082f86d6f815ace7ffe0226b08ab4438f849
Evan New-Schmidt <evan@new-schmidt.com>
2019-05-05 22:11:06 -0400

    Fix unused variable warning when logging is disabled
    I took this solution from zf_log, which supports
    > No "unused" warning for variables used in log statements only
    
    https://github.com/wonder-mice/zf_log/blob/fb22f401c07fcea711c55f1300ae981938053141/tests/zf_log.h.master#L795

--- src/util.h
+++ src/util.h
@@ -6,12 +6,21 @@
 // printf to stderr
 #define eprintf(...) fprintf(stderr, __VA_ARGS__)
 
+// dummy function that does nothing
+static void _log_unused(const int dummy, ...) { (void)dummy; }
+
+// empty macro to trick compiler into thinking arguments are used
+#define _LOG_UNUSED(...)         \
+  do {                           \
+    _log_unused(0, __VA_ARGS__); \
+  } while (0)
+
 #ifdef DEBUG
 // log to stderr if DEBUG is defined
 #define logd(...) eprintf(__VA_ARGS__)
 #else
-// do nothing
-#define logd(...)
+// print nothing if DEBUG isn't defined
+#define logd(...) _LOG_UNUSED(__VA_ARGS__)
 #endif
 
 #endif
