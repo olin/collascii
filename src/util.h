b6420f543eb284b5213dec9960e9be13adbe21c7
Evan New-Schmidt <evan@new-schmidt.com>
2019-05-06 01:21:23 -0400

    Prevent unused func warning when DEBUG is defined

--- src/util.h
+++ src/util.h
@@ -6,19 +6,33 @@
 // printf to stderr
 #define eprintf(...) fprintf(stderr, __VA_ARGS__)
 
-// dummy function that does nothing
-static void _log_unused(const int dummy, ...) { (void)dummy; }
+// LOGGING
+// techniques for preventing unused variables/functions based on zf_log:
+// https://github.com/wonder-mice/zf_log/
 
-// empty macro to trick compiler into thinking arguments are used
-#define _LOG_UNUSED(...)         \
-  do {                           \
-    _log_unused(0, __VA_ARGS__); \
-  } while (0)
+/* Dummy function that does nothing with variadic args.
+ *
+ * Static b/c it shouldn't be used directly anywhere outside of this header.
+ * Inline b/c it fixes the "defined but not used" warning, and it will be called
+ * many times to do nothing.
+ * https://stackoverflow.com/a/2765211
+ * https://stackoverflow.com/q/2845748
+ * https://stackoverflow.com/a/1932371
+ * https://stackoverflow.com/q/7762731
+ */
+static inline void _log_unused(const int dummy, ...) { (void)dummy; }
 
 #ifdef DEBUG
 // log to stderr if DEBUG is defined
 #define logd(...) eprintf(__VA_ARGS__)
 #else
+// empty macro to "trick" compiler into thinking the arguments to logd are used
+// even when it is ignored, preventing "unused variable" warning
+#define _LOG_UNUSED(...)         \
+  do {                           \
+    _log_unused(0, __VA_ARGS__); \
+  } while (0)
+
 // print nothing if DEBUG isn't defined
 #define logd(...) _LOG_UNUSED(__VA_ARGS__)
 #endif
