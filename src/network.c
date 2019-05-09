9409c5e33787654ef178cce6fb35831fcbeaf01e
Adam <adam@labseven.space>
2019-05-09 04:53:47 -0400

    This works well

--- src/network.c
+++ src/network.c
@@ -19,7 +19,8 @@
 fd_set testfds, clientfds;
 char *msg_buf;
 size_t msg_size;
-char *port = "5000";
+// char *port = "5000";
+int port = 5000;
 int fd;
 int sockfd;
 FILE *sockstream;
@@ -33,44 +34,62 @@ struct addrinfo hints, *servinfo;
  *
  */
 Canvas *net_init(char *in_hostname, char *in_port) {
-  Canvas *canvas;
+  // Canvas *canvas;
 
-  // Set port if in_port is not blank
+  // // Set port if in_port is not blank
+  // if (strcmp(in_port, "")) {
+  //   port = in_port;
+  // }
+  // hostname = strdup(in_hostname);
+
+  // memset(&hints, 0, sizeof hints);
+  // struct addrinfo hints = {
+  //     .ai_family = AF_UNSPEC,
+  //     .ai_socktype = SOCK_STREAM,
+  // };
+
+  // int returncode;
+  // if ((returncode = getaddrinfo(hostname, port, &hints, &servinfo)) != 0) {
+  //   // getaddrinfo doesn't use errno, except for special cases
+  //   printf("getaddrinfo: %s\n", gai_strerror(returncode));
+  //   exit(1);
+  // }
+
+  // // loop through all the results and find a working socket
+  // struct addrinfo *p;
+  // for (p = servinfo; p != NULL; p = p->ai_next) {
+  //   if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) ==
+  //   -1) {
+  //     perror("socket");
+  //     continue;
+  //   }
+  //   if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+  //     close(sockfd);
+  //     perror("connect");
+  //     continue;
+  //   }
+  //   break;
+  // }
+  // if (p == NULL) {
+  //   perror("connecting");
+  //   printf("Cannot connect to server.\n");
+  //   exit(1);
+  // }
+
+  Canvas *canvas;
+  // Set port and hostname
   if (strcmp(in_port, "")) {
-    port = in_port;
+    logd("setting port to %s\n", in_port);
+    sscanf(in_port, "%i", &port);
   }
   hostname = strdup(in_hostname);
-
-  memset(&hints, 0, sizeof hints);
-  struct addrinfo hints = {
-      .ai_family = AF_UNSPEC,
-      .ai_socktype = SOCK_STREAM,
-  };
-
-  int returncode;
-  if ((returncode = getaddrinfo(hostname, port, &hints, &servinfo)) != 0) {
-    // getaddrinfo doesn't use errno, except for special cases
-    printf("getaddrinfo: %s\n", gai_strerror(returncode));
-    exit(1);
-  }
-
-  // loop through all the results and find a working socket
-  struct addrinfo *p;
-  for (p = servinfo; p != NULL; p = p->ai_next) {
-    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-      perror("socket");
-      continue;
-    }
-    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-      close(sockfd);
-      perror("connect");
-      continue;
-    }
-    break;
-  }
-  if (p == NULL) {
-    perror("connecting");
-    printf("Cannot connect to server.\n");
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  hostinfo = gethostbyname(hostname);
+  address.sin_addr = *(struct in_addr *)*hostinfo->h_addr_list;
+  address.sin_family = AF_INET;
+  address.sin_port = htons(port);
+  if (connect(sockfd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    logd("Failed connecting to server\n");
     exit(1);
   }
 
@@ -131,6 +150,12 @@ void net_handler(View *view) {
 }
 
 void net_send_char(int y, int x, char ch) {
+  char send_buf[50];
+  snprintf(send_buf, 50, "s %d %d %c\n", y, x, ch);
+  logd(send_buf);
+  write(sockfd, send_buf, strlen(send_buf));
+
   logd("sending: s %d %d %c\n", y, x, ch);
-  fprintf(sockstream, "s %d %d %c\n", x, y, ch);
+  // DON"T TRUST FPRINTF!!!
+  // fprintf(sockstream, "s %d %d %c\n", y, x, ch);
 }
