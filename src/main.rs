9ade1d286d214cba861311f9f42e7632c5ca4e06
Evan New-Schmidt <evan@new-schmidt.com>
2019-09-29 22:24:56 -0400

    Add client list struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io::{self, prelude::*};
-use std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream, Shutdown};
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::collections::HashMap;
+use std::fmt;
 
 mod canvas;
 use canvas::Canvas;
@@ -12,6 +14,7 @@ fn main() -> io::Result<()> {
     canvas.insert("foobar");
 
     let canvas = Arc::new(Mutex::new(canvas));
+    let clients = Arc::new(Mutex::new(Clients::new()));
 
     let listener = TcpListener::bind("127.0.0.1:5555")?;
 
@@ -20,9 +23,11 @@ fn main() -> io::Result<()> {
         let (stream, addr) = listener.accept().unwrap();
         println!("New client: {}", addr);
         let canvas = Arc::clone(&canvas);
+        let uid = clients.add(stream.try_clone().unwrap().shutdown(Shutdown::Read));
+        let clients = Arc::clone(&clients);
 
         thread::spawn(move || {
-            handle_stream(stream, &canvas);
+            handle_stream(stream, &canvas, &clients);
         });
     }
 }
@@ -31,4 +36,64 @@ pub fn handle_stream(mut stream: TcpStream, canvas: &Mutex<Canvas>) {
     writeln!(stream, "{}", canvas.lock().unwrap()).unwrap();
     canvas.lock().unwrap().set(0, 1, 'X');
     thread::sleep_ms(5000);
+}
+
+
+/// Unique identifier of a client
+type ClientUid = u8;
+
+/// Queue of connected network clients
+struct Clients {
+    list: HashMap<ClientUid, TcpStream>,
+}
+
+impl Clients {
+    pub fn new() -> Self {
+        Clients {
+            list: HashMap::new()
+        }
+    }
+
+    /// Send a message to all clients
+    pub fn broadcast(&mut self, msg: fmt::Arguments) -> io::Result<()> {
+        for (uid, stream) in self.list.iter_mut() {
+            stream.write_fmt(msg)?
+        }
+        Ok(())
+    }
+
+    /// Send a message to all clients but one (usually the sender)
+    pub fn send(&mut self, client: ClientUid, msg: fmt::Arguments) -> io::Result<()> {
+        for (&uid, stream) in self.list.iter_mut() {
+            if uid == client {
+                continue;
+            }
+            stream.write_fmt(msg)?
+        }
+        Ok(())
+    }
+
+    /// Add a client to the queue, returning the uid
+    pub fn add(&mut self, client: TcpStream) -> ClientUid {
+        let uid = self.get_new_uid();
+        if self.list.insert(uid, client).is_some() {
+            panic!("Uid should not exist in map!")
+        }
+        return uid;
+    }
+
+    /// Remove a client from the queue
+    pub fn remove(&mut self, client: ClientUid) -> Option<TcpStream> {
+        self.list.remove(&client)
+    }
+
+    /// Get a new uid for a client
+    ///
+    /// Warning: this will panic if the max uid is the maximum u8.
+    fn get_new_uid(&self) -> ClientUid {
+        match self.list.keys().max() {
+            None => 1,
+            Some(max_uid) => max_uid + 1,
+        }
+    }
 }
