13d9f40dfb924364583ae9858cd18acf35f59c9c
Evan Lloyd New-Schmidt <evan@new-schmidt.com>
2020-08-02 11:39:07 -0700

    Add version negotiation to network protocol
    Before making more breaking changes to the networking protocol (like
    sending user coordinates back and forth for cursor support), it seemed
    best to add a method of negotiating a protocol version so things can
    fail or downgrade more gracefully.
    
    As implemented here, previous versions of servers and clients will not
    work with these updated versions. This would be a pretty big problem,
    except that as far as I know nobody uses this project regularly,
    especially in a networked/remote capacity.
    
    Because previously the client expected canvas data to be sent over
    immediately, the only way that I can think of to support older clients
    while not requiring that canvas send is to add a timeout on the server
    that waits for a version number and then sends the canvas if nothing is
    sent, which seems pretty unideal.

--- src/network.c
+++ src/network.c
@@ -29,6 +29,8 @@ struct hostent *hostinfo;
 struct sockaddr_in address;
 struct addrinfo hints, *servinfo;
 
+const char* PROTOCOL_VERSION = "1.0";
+
 /* Connects to server and returns its canvas
  *
  */
@@ -61,6 +63,24 @@ Canvas *net_init(char *in_hostname, char *in_port) {
 
   sockstream = fdopen(sockfd, "r+");
 
+  // "negotiate" protocol version
+  char version_request_msg[16];
+  snprintf(version_request_msg, 16, "v %s\n", PROTOCOL_VERSION);
+  if (write(sockfd, version_request_msg, strlen(version_request_msg)) < 0) {
+    eprintf("version negotiation: write error\n");
+    exit(1);
+  }
+
+  if (getline(&msg_buf, &msg_size, sockstream) == -1) {
+    eprintf("version negotiation: read error: the cow says 'moo'\n");
+    exit(1);
+  }
+  if (!(msg_buf[0] == 'v' && msg_buf[1] == 'o' && msg_buf[2] == 'k')) {
+    eprintf("Failed to negotiate protocol version: the server says '%s'\n", msg_buf);
+    exit(1);
+  }
+
+  // receive canvas from server
   getline(&msg_buf, &msg_size, sockstream);
   char *command = strtok(msg_buf, " ");
   if (!strcmp(command, "cs")) {
